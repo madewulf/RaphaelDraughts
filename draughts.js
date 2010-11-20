4001c16f40c0250a262af6c8b7b299827d31d060
madewulf <madewulf@gmail.com>
2010-11-20 19:53:27 +0100

    The code went through a little JSLint session

--- draughts.js
+++ draughts.js
@@ -2,24 +2,26 @@ window.onload = function() {
     var paper = new Raphael(document.getElementById('canvas_container'), 500, 500);
 
     var selectedPiece=null;
+    function movePiece(){
+        var at= this.attr();
+        selectedPiece.animate({cx:at.x+25,cy:at.y+25},1500,"bounce");
+    }
 	for(var x = 0; x < 10; x++) {
         for (var y = 0 ; y <10 ; y++){
-           var cx=x*50+25;
-           var cy=y*50+25;
-           var rect = paper.rect(x*50,y*50,50,50).click(function(){
-                var at= this.attr();
-                selectedPiece.animate({cx:at['x']+25,cy:at['y']+25},1500,"bounce");
-            });
+
+           var rect = paper.rect(x*50,y*50,50,50).click(movePiece);
 
            var attr;
-           if ((x+y) %2!=0)
+           if ((x+y) %2!==0)
            {
              attr={fill: '#000', stroke: '#666', 'stroke-width': 1} ;
              if (y!=4 && y!=5)
              {
                  var c= paper.circle(x*50+25,y*50+25,20).click(function () {
                       if (selectedPiece)
-                      selectedPiece.animate({stroke: '#666', 'stroke-width': 1},1000,'bounce');
+                      {
+                        selectedPiece.animate({stroke: '#666', 'stroke-width': 1},1000,'bounce');
+                      }
                       selectedPiece=this;
                       this.animate({stroke: 'orange', 'stroke-width': 3},1500,"bounce");
                  });
@@ -45,7 +47,7 @@ window.onload = function() {
            }
            else
            {
-              attr = {fill: '#fff', stroke: '#666', 'stroke-width': 1}
+              attr = {fill: '#fff', stroke: '#666', 'stroke-width': 1};
            }
 
            rect.attr(attr);
@@ -53,5 +55,5 @@ window.onload = function() {
            
         }
     }
-}
+};
         
